663fddb2ca8e0fb5592ee02593fc602221b2cc43
stonemonkey <stonemonkey77ro@yahoo.com>
2013-06-07 12:44:13 +0300

    first revision.

---
+++ GoL.Tests/UniverseTests.cs
@@ -0,0 +1,96 @@
+﻿using System.Drawing;
+using NUnit.Framework;
+
+namespace GoL.Tests
+{
+    public class UniverseTests
+    {
+        private Universe _universe;
+
+        [SetUp]
+        public void TestInitialize()
+        {
+            _universe = new Universe();
+        }
+        
+        [Test]
+        public void Tick_returns_empty_generation_when_cell_has_1_neighbour()
+        {
+            var cell = new Point(0, 0);
+            var neighbour = new Point(1, 0);
+
+            var generation = _universe.Tick(cell, neighbour);
+
+            CollectionAssert.DoesNotContain(generation, cell);
+        }
+
+        [Test]
+        public void Tick_returns_generation_with_cell_when_cell_has_2_neighbours()
+        {
+            var cell = new Point(0, 0);
+            var firstNeighbour = new Point(-1, 0);
+            var secondNeighbour = new Point(1, 1);
+
+            var generation = _universe.Tick(cell, firstNeighbour, secondNeighbour);
+
+            CollectionAssert.Contains(generation, cell);
+        }
+
+        [Test]
+        public void Tick_returns_generation_with_cell_when_cell_has_3_neighbours()
+        {
+            var cell = new Point(0, 0);
+            var firstNeighbour = new Point(-1, -1);
+            var secondNeighbour = new Point(-1, 1);
+            var thirdNeighbour = new Point(1, 0);
+
+            var generation = _universe.Tick(cell, firstNeighbour, secondNeighbour, thirdNeighbour);
+
+            CollectionAssert.Contains(generation, cell);
+        }
+
+        [Test]
+        public void Tick_returns_generation_without_cell_when_cell_has_4_neighbours()
+        {
+            var cell = new Point(0, 0);
+            var firstNeighbour = new Point(-1, -1);
+            var secondNeighbour = new Point(-1, 1);
+            var thirdNeighbour = new Point(1, 0);
+            var fourthNeighbour = new Point(1, 1);
+
+            var generation = _universe.Tick(cell, firstNeighbour, secondNeighbour, thirdNeighbour, fourthNeighbour);
+
+            CollectionAssert.DoesNotContain(generation, cell);
+        }
+
+        [Test]
+        public void Tick_returns_generation_with_allive_cell_when_cell_was_dead_and_had_3_allive_neighbours()
+        {
+            var cell = new Point(0, 0);
+            var firstNeighbour = new Point(-1, -1);
+            var secondNeighbour = new Point(0, -1);
+            var thirdNeighbour = new Point(1, -1);
+
+            var generation = _universe.Tick(firstNeighbour, secondNeighbour, thirdNeighbour);
+
+            CollectionAssert.Contains(generation, cell);
+        }
+
+        // This test is not necessary but I just wanted to prove Blinker works as expected.
+        [Test]
+        public void Tick_returns_blinker()
+        {
+            var cell = new Point(0, 0);
+            var firstNeighbour = new Point(-1, 0);
+            var secondNeighbour = new Point(1, 0);
+
+            var generation = _universe.Tick(cell, firstNeighbour, secondNeighbour);
+
+            CollectionAssert.Contains(generation, cell);
+            CollectionAssert.Contains(generation, new Point(0, -1));
+            CollectionAssert.Contains(generation, new Point(0, 1));
+            CollectionAssert.DoesNotContain(generation, firstNeighbour);
+            CollectionAssert.DoesNotContain(generation, secondNeighbour);
+        }
+    }
+}
---
+++ GoL/Universe.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace GoL
+{
+    public class Universe
+    {
+        private List<Point> _inputGeneration;
+        private readonly List<Point> _nextGeneration;
+
+        public Universe()
+        {
+            _nextGeneration = new List<Point>();
+        }
+
+        public IList<Point> Tick(params Point[] inputGeneration)
+        {
+            _inputGeneration = new List<Point>(inputGeneration);
+
+            foreach (var cell in inputGeneration)
+                ProcessCell(cell);
+
+            return _nextGeneration;
+        }
+
+        private void ProcessCell(Point cell)
+        {
+            KeepCellAlive(cell);
+
+            ResurectDeadNeighbours(cell);
+        }
+
+        private void ResurectDeadNeighbours(Point cell)
+        {
+            var deadNeighbours = GetDeadNeighbours(cell);
+
+            foreach (var deadNeighbour in deadNeighbours)
+                ResurectDeadCell(deadNeighbour);
+        }
+
+        private void ResurectDeadCell(Point deadNeighbour)
+        {
+            var aliveNeighbours = GetAlliveNeighbours(deadNeighbour)
+                .ToList();
+            
+            if (IsGoodWillToGetAllive(aliveNeighbours))
+                _nextGeneration.Add(deadNeighbour);
+        }
+
+        private void KeepCellAlive(Point cell)
+        {
+            if (HasNumberOfValidNeighbours(cell))
+                _nextGeneration.Add(cell);
+        }
+
+        private static bool IsGoodWillToGetAllive(
+            ICollection aliveNeighbours)
+        {
+            return (aliveNeighbours.Count == 3);
+        }
+
+        private IEnumerable<Point> GetDeadNeighbours(Point cell)
+        {
+            var allNeighbours = GetAllNeighbours(cell);
+            var aliveNeighbours = GetAlliveNeighbours(cell);
+            
+            return allNeighbours.Where(
+                c => !aliveNeighbours.Contains(c));
+        }
+
+        private static IEnumerable<Point> GetAllNeighbours(Point cell)
+        {
+            for(var x = -1; x <= 1; x++)
+                for (var y = -1; y <= 1 ; y++)
+                    yield return new Point(cell.X + x, cell.Y + y);
+        }
+
+        private bool HasNumberOfValidNeighbours(Point cell)
+        {
+            var neighbours = GetAlliveNeighbours(cell).ToList();
+
+            return (neighbours.Count == 2) || (neighbours.Count == 3);
+        }
+
+        private IEnumerable<Point> GetAlliveNeighbours(Point cell)
+        {
+            return _inputGeneration.Where(
+                c => (Math.Abs(cell.X - c.X) < 2) && 
+                     (Math.Abs(cell.Y - c.Y) < 2) && 
+                     (c != cell));
+        }
+    }
+}
